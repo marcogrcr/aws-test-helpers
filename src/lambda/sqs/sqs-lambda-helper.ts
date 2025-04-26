1f2698e9b2673f4051eeb149047f5d7a525699cc
Marco Gonzalez <marcogrcr@gmail.com>
2025-04-26 01:41:07 -0400

    feat: sqs lambda helper

---
+++ src/lambda/sqs/sqs-lambda-helper.ts
@@ -0,0 +1,195 @@
+import { GetQueueUrlCommand, type Message } from "@aws-sdk/client-sqs";
+import type { AbortSignal } from "@aws-sdk/types";
+import type { SQSEvent, SQSHandler } from "aws-lambda";
+
+import { type Logger, NoLogger } from "../../logger";
+import { SqsHelper } from "../../sqs/sqs-helper";
+import { LambdaHelper } from "..";
+
+export interface RunSqsLambdaInput {
+  /** An abort signal. */
+  readonly abortSignal?: AbortSignal;
+
+  /** The batch size. */
+  readonly batchSize: number;
+
+  /** The AWS endpoint URL. */
+  readonly endpoint: string;
+
+  /** The Lambda handler. */
+  readonly handler: SQSHandler;
+
+  /** The logger. */
+  readonly logger?: Logger;
+
+  /** The SQS queue. */
+  readonly queue:
+    | {
+        /** The SQS queue name. */
+        readonly name: string;
+      }
+    | {
+        /** The SQS queue url. */
+        readonly url: string;
+      };
+
+  /** The Lambda timeout in seconds. */
+  readonly timeout: number;
+}
+
+export interface ToSqsEventInput {
+  /**
+   * The AWS account ID.
+   * @default "123456789012"
+   */
+  readonly account?: string;
+
+  /** The SQS messages. */
+  readonly messages: readonly Message[];
+
+  /**
+   * The SQS queue name.
+   * @default "queue"
+   */
+  readonly queueName?: string;
+
+  /**
+   * The AWS region.
+   * @default process.env.AWS_REGION ?? "us-east-1"
+   */
+  readonly region?: string;
+}
+
+export interface ToSqsEventOutput {
+  /** The SQS event. */
+  readonly event: SQSEvent;
+}
+
+/** Provides utilities for AWS Lambda handler functions that consume {@link SQSEvent}. */
+export class SqsLambdaHelper {
+  private constructor() {}
+
+  /** Simulates the Lambda service invoking a {@link SQSHandler}. */
+  static async runSqsLambda(input: RunSqsLambdaInput): Promise<void> {
+    const {
+      abortSignal,
+      batchSize,
+      endpoint,
+      handler,
+      logger = new NoLogger(),
+      queue,
+      timeout,
+    } = input;
+
+    // get queue URL
+    let queueUrl: string;
+    if ("name" in queue) {
+      const { QueueUrl } = await SqsHelper.send({
+        command: new GetQueueUrlCommand({
+          QueueName: queue.name,
+        }),
+        endpoint,
+      });
+      queueUrl = QueueUrl!;
+    } else {
+      queueUrl = queue.url;
+    }
+
+    // poll/invoke loop
+    while (abortSignal === undefined || !abortSignal.aborted) {
+      // poll SQS queue
+      logger.info("Polling for SQS messages...");
+      let messages: readonly Message[];
+      try {
+        const res = await SqsHelper.getMessages({
+          abortSignal,
+          batchSize,
+          endpoint,
+          queueUrl,
+        });
+        messages = res.messages;
+      } catch (error) {
+        if (error instanceof Error && error.name === "AbortError") {
+          break;
+        }
+        throw error;
+      }
+
+      // invoke Lambda handler
+      logger.info(`Invoking lambda handler with ${messages.length} messages.`);
+      const output = await LambdaHelper.invokeLambda({
+        event: SqsLambdaHelper.toSqsEvent({ messages }).event,
+        handler,
+        timeout,
+      });
+      if (output.success) {
+        logger.info("Successfully invoked lambda handler.", {
+          result: output.result,
+        });
+      } else {
+        logger.error("Failed to invoke lambda handler.", {
+          error: output.error,
+          timeout: output.timeout,
+        });
+      }
+    }
+  }
+
+  /** Converts {@link Message} instances to an {@link SQSEvent}. */
+  static toSqsEvent(input: ToSqsEventInput): ToSqsEventOutput {
+    const {
+      account = "123456789012",
+      messages,
+      queueName = "queue",
+      region = process.env.AWS_REGION ?? "us-east-1",
+    } = input;
+
+    return {
+      event: {
+        Records: messages.map((m) => ({
+          attributes:
+            m.Attributes as unknown as SQSEvent["Records"][0]["attributes"],
+          awsRegion: region,
+          eventSource: "aws:sqs",
+          eventSourceARN: `arn:aws:sqs:${region}:${account}:${queueName}`,
+          body: m.Body!,
+          md5OfBody: m.MD5OfBody!,
+          messageAttributes: m.MessageAttributes
+            ? Object.fromEntries(
+                Object.entries(m.MessageAttributes).map(
+                  ([
+                    key,
+                    {
+                      BinaryListValues,
+                      BinaryValue,
+                      DataType,
+                      StringListValues,
+                      StringValue,
+                    },
+                  ]) => [
+                    key,
+                    {
+                      binaryValue: BinaryValue
+                        ? Buffer.from(BinaryValue).toString("base64")
+                        : undefined,
+                      binaryListValues: (BinaryListValues
+                        ? BinaryListValues.map((b) =>
+                            Buffer.from(b).toString("base64"),
+                          )
+                        : undefined) as SQSEvent["Records"][0]["messageAttributes"][string]["binaryListValues"],
+                      dataType: DataType!,
+                      stringValue: StringValue,
+                      stringListValues:
+                        StringListValues as SQSEvent["Records"][0]["messageAttributes"][string]["stringListValues"],
+                    },
+                  ],
+                ),
+              )
+            : {},
+          messageId: m.MessageId!,
+          receiptHandle: m.ReceiptHandle!,
+        })),
+      },
+    };
+  }
+}
---
+++ test/lambda/sqs/sqs-lambda-helper.test.ts
@@ -0,0 +1,204 @@
+import { randomUUID } from "node:crypto";
+
+import { AbortController } from "@aws-sdk/abort-controller";
+import {
+  CreateQueueCommand,
+  DeleteQueueCommand,
+  SendMessageCommand,
+} from "@aws-sdk/client-sqs";
+import type { SQSHandler } from "aws-lambda";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { SqsLambdaHelper } from "../../../src/lambda/sqs/sqs-lambda-helper";
+import { ConsoleLogger } from "../../../src/logger";
+import { SqsHelper } from "../../../src/sqs";
+import { delay } from "../../util";
+
+const ENDPOINT = process.env.AWS_ENDPOINT!;
+vi.mock("../../../src/logger");
+
+describe("SqsLambdaHelper", () => {
+  describe("runSqsLambda", () => {
+    let queueName: string;
+    let queueUrl: string;
+    beforeEach(async () => {
+      queueName = randomUUID();
+      const res = await SqsHelper.upsertQueue({
+        command: new CreateQueueCommand({
+          QueueName: queueName,
+        }),
+        endpoint: ENDPOINT,
+      });
+      queueUrl = res.queueUrl;
+
+      await Promise.all(
+        Array.from({ length: 2 }).map((_, id) =>
+          SqsHelper.send({
+            command: new SendMessageCommand({
+              MessageBody: id.toString(),
+              QueueUrl: queueUrl,
+            }),
+            endpoint: ENDPOINT,
+          }),
+        ),
+      );
+    });
+
+    it("runs lambda", async () => {
+      const abortController = new AbortController();
+      const error = new Error("failure");
+      let invocationCount = 0;
+
+      await SqsLambdaHelper.runSqsLambda({
+        abortSignal: abortController.signal,
+        batchSize: 1,
+        endpoint: ENDPOINT,
+        handler: async (event) => {
+          if (++invocationCount === 2) {
+            abortController.abort();
+          }
+
+          if (event.Records[0].body === "0") {
+            throw error;
+          } else {
+            return "success" as unknown as ReturnType<SQSHandler>;
+          }
+        },
+        logger: new ConsoleLogger(),
+        queue: { name: queueName },
+        timeout: 3,
+      });
+
+      expect(ConsoleLogger.prototype.error).toHaveBeenCalledWith(
+        "Failed to invoke lambda handler.",
+        { error, timeout: false },
+      );
+      expect(ConsoleLogger.prototype.info).toHaveBeenCalledWith(
+        "Successfully invoked lambda handler.",
+        { result: "success" },
+      );
+    });
+
+    describe("abortSignal aborted", async () => {
+      it("aborts SQS polling", async () => {
+        const abortController = new AbortController();
+        const handler = vi.fn();
+
+        await Promise.all([
+          SqsLambdaHelper.runSqsLambda({
+            abortSignal: abortController.signal,
+            batchSize: 3,
+            endpoint: ENDPOINT,
+            handler,
+            queue: { url: queueUrl },
+            timeout: 3,
+          }),
+          (async () => {
+            await delay(1);
+            abortController.abort();
+          })(),
+        ]);
+
+        expect(handler).not.toHaveBeenCalled();
+      });
+    });
+
+    describe("fail to poll SQS", () => {
+      it("throws error", async () => {
+        await SqsHelper.send({
+          command: new DeleteQueueCommand({
+            QueueUrl: queueUrl,
+          }),
+          endpoint: ENDPOINT,
+        });
+        const handler = vi.fn();
+
+        const actual = SqsLambdaHelper.runSqsLambda({
+          batchSize: 3,
+          endpoint: ENDPOINT,
+          handler,
+          queue: { url: queueUrl },
+          timeout: 3,
+        });
+
+        await expect(actual).rejects.toThrow();
+        expect(handler).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe("toSqsEvent", () => {
+    it("converts messages to an SQS event", () => {
+      const actual = SqsLambdaHelper.toSqsEvent({
+        account: "account",
+        messages: [
+          {
+            Attributes: {
+              foo: "bar",
+            },
+            Body: "body",
+            MD5OfBody: "body-md5",
+            MD5OfMessageAttributes: "attr-md5",
+            MessageAttributes: {
+              binaryListValues: {
+                BinaryListValues: [Buffer.from("binary"), Buffer.from("list")],
+                DataType: "Binary",
+              },
+              binaryValue: {
+                BinaryValue: Buffer.from("binary"),
+                DataType: "Binary",
+              },
+              stringListValues: {
+                DataType: "String",
+                StringListValues: ["string", "list"],
+              },
+              stringValue: {
+                DataType: "String",
+                StringValue: "string",
+              },
+            },
+            MessageId: "message-id",
+            ReceiptHandle: "receipt-handle",
+          },
+        ],
+        queueName: "queue-name",
+        region: "region",
+      });
+
+      expect(actual).toStrictEqual({
+        event: {
+          Records: [
+            {
+              attributes: { foo: "bar" },
+              awsRegion: "region",
+              eventSource: "aws:sqs",
+              eventSourceARN: "arn:aws:sqs:region:account:queue-name",
+              body: "body",
+              md5OfBody: "body-md5",
+              messageAttributes: {
+                binaryListValues: expect.objectContaining({
+                  binaryListValues: ["YmluYXJ5", "bGlzdA=="],
+                  dataType: "Binary",
+                }),
+                binaryValue: expect.objectContaining({
+                  dataType: "Binary",
+                  binaryValue: "YmluYXJ5",
+                }),
+                stringListValues: expect.objectContaining({
+                  dataType: "String",
+                  stringListValues: ["string", "list"],
+                }),
+                stringValue: expect.objectContaining({
+                  dataType: "String",
+                  stringValue: "string",
+                }),
+              },
+              messageId: "message-id",
+              receiptHandle: "receipt-handle",
+            },
+          ],
+        },
+      });
+    });
+  });
+});
